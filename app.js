6757ad8ab6e0974a7e3a0dec9b2f65f16f015127
aobasar <9959180+aobasar@users.noreply.github.com>
2023-05-15 16:20:13 -0700

    port update

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser')
 
 // Create an Express app
 const app = express()
-const port = 3001 || process.env.PORT
+const port = process.env.PORT || 3002
 
 // Use body-parser middleware to parse incoming request bodies
 app.use(bodyParser.urlencoded({ extended: false }));
